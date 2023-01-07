e41870fcbf1be19c4858e0c0d08059b0bc2e5b6f
navneet <navneetrajput246725@gmail.com>
2023-01-07 19:13:00 +0530

    initial

--- script.js
+++ script.js
@@ -8,20 +8,41 @@ let arr = [
 
 function PrintDeveloperswithMap() {
   //Write your code here
+  arr.map((item)=> {
+    if (item.profession === 'developer'){
+      console.log(item.name)
+    }
+  })
 }
 
 function PrintDeveloperbyForEach() {
   //Write your code here
+  arr.forEach((item)=>{
+    if (item.profession === 'developer'){
+      console.log(item.name)
+    }
+  })
 }
 
 function addData() {
   //Write your code here
+  arr.push({id:4,name:"susan",age:"20",profession:"intern"});
+  console.log(arr)
 }
 
 function removeAdmin() {
   //Write your code here
+  ar = arr.filter((item)=>item.profession!=='admin')
+  console.log(ar)
 }
 
 function concatenateArray() {
   //Write your code here
+  let arr2 = [
+    { id: 5, name: "Navneet", age: "24", profession: "Engineer" },
+    { id: 6, name: "Abhishek", age: "23", profession: "Politician" },
+    { id: 7, name: "Mudit", age: "22", profession: "Baklol" },
+  ];
+  let concateArray = arr.concat(arr2);
+  console.log(concateArray)
 }
